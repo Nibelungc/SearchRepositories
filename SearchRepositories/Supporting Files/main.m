8772cc3ad58efa8d8d508e71315d53c870b04153
Nikolay Kagala <nibelungc@gmail.com>
2016-07-11 10:11:20 +0800

    Create an Xcode Project

---
+++ SearchRepositories/Supporting Files/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SearchRepositories
+//
+//  Created by Nikolay Kagala on 11/07/16.
+//  Copyright © 2016 Nikolay Kagala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
